8daa034019e86a94c1425fbcff11bb916cfdf79e
Lucas <lucas.eartsup@gmail.com>
2023-03-17 10:07:49 +0100

    Project

---
+++ source/games/scenes/WorldScene.lua
@@ -0,0 +1,8 @@
+WorldScene = {}
+class("WorldScene").extends(NobleScene)
+
+WorldScene.baseColor = Graphics.kColorWhite
+
+function WorldScene:init()
+    WorldScene.super.init(self)
+end
---
+++ source/main.lua
@@ -0,0 +1,11 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+import "libraries/noble/Noble"
+import "libraries/animatedSprite/AnimatedSprite.lua"
+import "games/scenes/simpleScene.lua"
+import "games/scenes/WorldScene.lua"
+
+
+Noble.new(SimpleScene)
